2068d87f96d663170cd89583773e6c9c715778eb
Hayk <khachike@gmail.com>
2024-01-31 21:08:33 +0700

    Add-2

--- logistic/tests.py
+++ logistic/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from unittest import TestCase
 
-# Create your tests here.
+from rest_framework.test import APIClient
+
+
+class MyTest(TestCase):
+    def test_ok(self):
+        assert True
+
+    def test_sample_view(self):
+        url = "/api/v1/test/"
+        client = APIClient()
+        response = client.get(url)
+        assert response.status_code == 200
